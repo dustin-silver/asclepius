76149c956c83413c2abb004615d6ddf263bf7fca
Dustbin <defender2k@gmail.com>
2025-10-17 20:52:24 -0500

    Switched to redom and did some stuff

--- docs/main.js
+++ docs/main.js
@@ -1,95 +1,67 @@
-var rows, button_add_row;
+const { html, mount } = redom;
 window.onload = function () {
-  rows = document.getElementById("rows");
-  button_add_row = document.getElementById("add_row");
-  button_add_row.onclick = addRow;
+  mount(document.body, new ABO());
 };
-function newElement(type) {
-  return document.createElement(type);
-}
-function newInput(type) {
-  let i = newElement("input");
-  i.type = type;
-  return i;
-}
-function newSelect(options) {
-  let s = newElement("select");
-  for (i in options) {
-    let o = newElement("option");
-    o.value = i;
-    o.innerHTML = options[i];
-    s.appendChild(o);
+class ABO {
+  static none = "None";
+  static major = "Major";
+  static minor = "Minor";
+  static bidirectional = "Bidirectional";
+  constructor() {
+    this.el = html(".abo", [
+      html(
+        ".donor",
+        "Donor",
+        html("span", [
+          (this.donor = aboSelector()),
+          (this.donorRh = rhCheckbox()),
+        ]),
+      ),
+      html(
+        ".recipient",
+        "Recipient",
+        html("span", [
+          (this.recipient = aboSelector()),
+          (this.recipientRh = rhCheckbox()),
+        ]),
+      ),
+      html(".result", "Result", (this.result = html("span"))),
+    ]);
+    this.donor.onchange = () => {
+      this.setResult();
+    };
+    this.recipient.onchange = () => {
+      this.setResult();
+    };
   }
-  return s;
-}
-function newABOObject() {
-  let ret = {};
-  let span = newElement("span");
-  span.className = "span_abo";
-  let select = newSelect({ a: "A", b: "B", ab: "AB", o: "O" });
-  select.onchange = function () {
-    ret.onchange();
-  };
-  span.appendChild(select);
-  let label = newElement("label");
-  label.className = "check_rh";
-  let checkbox = newInput("checkbox");
-  checkbox.onchange = function () {
-    ret.onchange();
-  };
-  label.appendChild(checkbox);
-  span.appendChild(label);
-  ret.span = span;
-  ret.abo = select;
-  ret.rh = checkbox;
-  return ret;
-}
-function addRow() {
-  let row = {};
-  let form = newElement("form");
-  form.className = "table_row";
-
-  let span = newElement("span");
-  let input = newInput("text");
-  input.name = "input_label";
-  span.appendChild(input);
-  form.appendChild(span);
-
-  row.donor = newABOObject();
-  row.donor.onchange = function () {
-    typeChange(row);
-  };
-  form.appendChild(row.donor.span);
-
-  row.recipient = newABOObject();
-  row.recipient.onchange = function () {
-    typeChange(row);
-  };
-  form.appendChild(row.recipient.span);
-
-  row.result = newElement("span");
-  form.appendChild(row.result);
-
-  typeChange(row);
-  rows.appendChild(form);
-}
-
-function typeChange(row) {
-  switch (row.recipient.abo.value) {
-    case "o":
-      if (row.donor.abo.value == "o") row.result.innerHTML = "None";
-      else row.result.innerHTML = "Major";
-      break;
-    case "ab":
-      if (row.donor.abo.value == "ab") row.result.innerHTML = "None";
-      else row.result.innerHTML = "Minor";
-      break;
-    case "a":
-    case "b":
-      if (row.donor.abo.value == row.recipient.abo.value)
-        row.result.innerHTML = "None";
-      else if (row.donor.abo.value == "ab") row.result.innerHTML = "Major";
-      else if (row.donor.abo.value == "o") row.result.innerHTML = "Minor";
-      else row.result.innerHTML = "Major and minor";
+  setResult() {
+    if (this.donor.value == this.recipient.value) {
+      this.result.innerHTML = ABO.none;
+      return;
+    }
+    switch (this.recipient.value) {
+      case "o":
+        this.result.innerHTML = ABO.major;
+        break;
+      case "ab":
+        this.result.innerHTML = ABO.minor;
+        break;
+      case "a":
+      case "b":
+        if (this.donor.value == "ab") this.result.innerHTML = ABO.major;
+        else if (this.donor.value == "o") this.result.innerHTML = ABO.minor;
+        else this.result.innerHTML = ABO.bidirectional;
+    }
   }
+}
+function aboSelector() {
+  return html("select", [
+    html("option", { value: "a" }, "A"),
+    html("option", { value: "b" }, "B"),
+    html("option", { value: "ab" }, "AB"),
+    html("option", { value: "o" }, "O"),
+  ]);
+}
+function rhCheckbox() {
+  return html("label.check_rh", html("input", { type: "checkbox" }));
 }
